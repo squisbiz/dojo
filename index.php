b0252c0031c294d5a11c4343380176062789fc6e
squisbiz <arturoruiz@Arturos-MacBook-Pro.local>
2015-09-18 11:29:27 -0700

    Initial Commit
    Initial theme structure

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/css.css">
+	<title>Nearsoft English Dojo</title>
+	<meta charset="UTF-8">
+	
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/dojo.js"></script>
+</head>
+
+<body>
+<div class="main-container">
+<div class="nav">
+	<div class="safearea">
+		<div class="logo"></div>
+		<ul>
+<!-- 			<li><a href="#resources">Resources</a></li> -->
+			<li><a href="#register">Register</a></li>
+      <li><a href="#live">Live!</a></li>
+      <li><a href="#programs">Our Programs</a></li>
+			<li><a href="#about">About</a></li>
+		</ul>
+	</div>
+</div>
+
+	<div id="main">
+		<div class="main-centered">
+			<div class="logo"></div>
+			<div class="tagline">
+				<p>Tools and resources for software engineers to improve English-based communication and soft skills.<br/>
+  				Weekly live streaming talks with <b>special guests</b>.
+				</p>
+				<a class="btn" href="#about">Learn More</a>
+			</div>
+		</div><!-- main-centered -->
+	</div>
+	
+	<div id="about">
+  	<h2>WHY THE DOJO?</h2>
+		<div class="about-centered">
+			<div class="tagline">
+				<p>According to Wikipedia, A dojo is a Japanese term which literally means "place of the way".<br>
+In the Western World, the term dojo primarily refers to a training place, usually meant for Japanese martial arts. The term can also refer to a formal training place for any of the Japanese arts ending in "do", meaning "way".<br><br>
+Inspired by this idea, and by the perfection-seeking performance of the martial artists, we have named this site The Dojo, a community-building online meeting place and training venue for software engineers to practice and hone their English-based communication and soft skills in a professional context. Just like a martial artists would do at a dojo.<br><br>
+The Dojo has been conceived for those who already have conversational command of the English language. It contains tools and resources to help software engineers and other professionals to help them hone their communication and soft skills, and also features weekly live streaming talks with special guests.</p>
+			</div>
+
+		</div><!-- about-centered -->
+		
+		
+		<h2>ABOUT US</h2>
+		<div class="about-centered">
+			<div class="tagline">
+				<p><img src="img/character.png" />The Dojo is a community-building online meeting place and training venue for software engineers to practice and hone their English-based communication and soft skills in a professional context.</p>
+				
+				<p>The Dojo has been conceived for those who already have conversational command of the English language. It contains tools and resources to help software engineers and other professionals to help them hone their communication and soft skills, and also features weekly live streaming talks with special guests.</p>
+				
+				<p>As a leading software company in Mexico catering to Silicon Valley-based clients, English comprises a good chunk of our very bread and butter. To make sure our communication skills remain seamless and up to par with the highest standards, we have created The Dojo, which started out simply as an online conversational club.  Soon thereafter, shorter 30 –minute, 1-on-1 sessions were implemented as a comprehensive English for Special Purposes and Soft Skills training, coaching our developers in English language skills, business etiquette, team building, speaking, presenting, client interaction, and cross-cultural sensitivity.</p>
+				
+				<p>Now, The Dojo also features DojoLive, a weekly online talk where engineers and other professionals gather to chat, practice English, learn about cultural awareness, hone their soft skills and learn industry insights from leaders and company executives in North America and other countries.</p>
+			</div>
+
+		</div><!-- about-centered -->
+		
+	</div>
+	
+	<div id="programs">
+  	<h2>OUR PROGRAMS</h2>
+		<div class="programs-centered">
+			
+			<div class="programs-left">
+  			<img src="img/img01.jpg" />
+			</div>
+			<div class="programs-right">
+				<h2>The 1-on-1 <i>Dojitos</i></h2>
+				<h3>... I already speak English! Why should I even bother and why is important?</h3>
+				<p>In the ever-growing and evolving software development industry, communication skills are increasingly becoming more stringent. We believe that clients deserve the best, and that includes our own communication capabilities.</p>
+				<p><i>How well honed are yours?</i></p>
+				<p>Personalized, 1-on-1 English/Soft Skills coaching sessions have successfully proven to be a great tool to enhance communication and soft skills with teammates, and – most importantly – with the clients. Sometimes, this is even required by the latter in order to move forward on a specific aspect of a project or team member, or to build trust on our end by proving our multicultural view beyond simply being able to read, understand or speak a foreign language in a high-end software engineering environment.</p>
+
+			</div>
+		</div>
+		
+		
+		<div class="programs-centered">
+			
+			<div class="programs-left">
+  			<img src="img/img02.png" />
+			</div>
+			<div class="programs-right">
+				<h2>In company and online customized communication enhacement</h2>
+				<p>Personalized Communication Training for Software Engineers
+Have specific needs in English-based communication and soft skills training?</i></p>
+        <h3>Send us your info, we’ll get right back to you.</h3>
+      
+        <a href="#register" class="button">Register now!</a>
+        
+      </div>
+		</div>
+	</div>
+	
+	
+	<a name="register"></a>
+	<div id="register">
+		<div class="register-centered">
+  		<h1>Save your spot, register now!</h1>
+  		<div class="register form">
+  		<form name="htmlform" method="post" action="send.php">
+    		
+    		<p class="error"></p>
+          <p><input type="text" name="name" placeholder="Your name"></p>
+          <p><input type="text" name="email" placeholder="E-mail"></p>
+          <p><input type="text" name="url" placeholder="company url"></p>
+          <p class="center"><input type="submit" value="Send request" class="smallbutton"></p>
+        <p class="center small">All information is kept private</p>
+				</form>
+
+			</div>
+		</div>
+	</div>
+  
+  
+  <div id="live">
+    <h1>Watch previous episodes</h1>
+    <div class="container">
+      <div class="prev-episode">
+        <a target="_blank" href="https://www.youtube.com/watch?v=RFvrVrtitR0"><img src="img/img04.png"/></a>
+        <h2><a href="https://www.linkedin.com/in/robert-graham">Robert Graham</a></h2>
+        <span>Business Hacker at Poll Everywhere</span>
+      </div>
+      <div class="prev-episode">
+        <a target="_blank" href="https://www.youtube.com/watch?v=x1ZEI8mH47g"><img src="img/img05.png" /></a>
+        <h2><a href="https://www.linkedin.com/in/kevindaum">Kevin Daum</a></h2>
+        <span>Columnist at Inc.com Author of the Amazon #1 Best Sellers Video Marketing For Dummies</span>
+      </div>
+      <div class="prev-episode">
+        <a target="_blank" href="https://www.youtube.com/watch?v=g2zXoFWUzXg"><img src="img/img06.png" /></a>
+        <h2><a href="https://www.linkedin.com/in/jorgeolson">Jorge S. Olson</a></h2>
+        <span>Business Hacker at Poll Everywhere</span>
+      </div>
+    </div>
+  </div>
+  
+
+  <div id="social">
+    <h2>Follow us on the internet</h2>
+    <div class="social-center">
+      <a class="sociallink fb" target="_blank"></a>
+      <a class="sociallink tw" target="_blank"></a>
+      <a class="sociallink li" target="_blank" href="https://www.linkedin.com/grp/home?gid=6571818"></a>
+      <a class="sociallink gp" target="_blank" href="https://plus.google.com/u/1/109277262538626079632/about?cfem=1"></a>
+      <a class="sociallink yt" target="_blank" href="https://www.youtube.com/channel/UCa_uwhMRbNhoV4tPxE_AlfQ"></a>
+    </div>
+  </div>
+  
+</div><!-- main-container -->
+</body>
+</html>
+
+
